5cceeabfbc9d8988384eee24c341be86789d126f
CN105 <cn105@CN0105.local>
2019-09-24 17:07:19 +0100

    cleaned up code

--- app.js
+++ app.js
@@ -7,24 +7,6 @@ app.use(express.static(path.join(__dirname, "public")))
 
 
 
-// // request -- responce
-// app.get('/' , (req,res) =>{
-//     res.send('<h1>Hello my name is Bob Hoskins</h1>')   // responce 
-// })
-
-
-// app.get('/blah' , (req,res) =>{
-//     console.log('blah')  
-//     // GET request to the url 
-//     // wont serve a responce on browser, but when you got to localhost:3000/thisWontGoToTheBrowser
-//     // will get 'blah' on the console
-// })
-
-
-// app.get('/info' , (req,res) =>{
-//     res.send({name:"tom" , age:"99"})   // automaticaly pasred as JSON data !
-// })
-
 
 console.log(__dirname)
 
